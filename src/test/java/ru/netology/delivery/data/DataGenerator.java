6633dd6f2fd9c0e01231fc58a695df3c8353362c
Kate Bo <bodrakea@gmail.com>
2024-02-15 19:28:32 +0300

    add test data, DeliveryTest

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -2,36 +2,51 @@ package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
 
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
-import java.util.Random;
+import java.util.*;
 
 public class DataGenerator {
+    static final String dateFormat = "dd.MM.yyyy";
+    static List<String> cities = new ArrayList<>();
     private DataGenerator() {
     }
 
     public static String generateDate(int shift) {
-        // TODO: добавить логику для объявления переменной date и задания её значения, для генерации строки с датой
-        // Вы можете использовать класс LocalDate и его методы для получения и форматирования даты
+        // логика генерации строки с датой
+        String date = LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern(dateFormat));
         return date;
     }
 
-    public static String generateCity(String locale) {
-        // TODO: добавить логику для объявления переменной city и задания её значения, генерацию можно выполнить
-        // с помощью Faker, либо используя массив валидных городов и класс Random
+    public static void generateListCities() throws IOException{
+        try (BufferedReader fileReader = new BufferedReader(new FileReader("src/test/resources/city.csv", StandardCharsets.UTF_8))){
+            String line;
+            while ((line = fileReader.readLine()) != null) {
+                cities.add(line);
+            }
+        }
+    }
+    public static String generateCity(String locale) throws IOException {
+        // генерация города их списка
+        generateListCities();
+        Random random = new Random();
+        String city = cities.get(random.nextInt(cities.size()));
         return city;
     }
 
     public static String generateName(String locale) {
-        // TODO: добавить логику для объявления переменной name и задания её значения, для генерации можно
-        // использовать Faker
+        // генерация фамилия + имя
+        Faker faker = new Faker(new Locale(locale));
+        String name = faker.name().lastName() + " " + faker.name().firstName();
         return name;
     }
 
     public static String generatePhone(String locale) {
-        // TODO: добавить логику для объявления переменной phone и задания её значения, для генерации можно
-        // использовать Faker
+        // генерация номера телефона
+        Faker faker = new Faker(new Locale(locale));
+        String phone = faker.phoneNumber().phoneNumber();
         return phone;
     }
 
@@ -39,17 +54,31 @@ public class DataGenerator {
         private Registration() {
         }
 
-        public static UserInfo generateUser(String locale) {
-            // TODO: добавить логику для создания пользователя user с использованием методов generateCity(locale),
-            // generateName(locale), generatePhone(locale)
+        public static UserInfo generateUser(String locale) throws IOException {
+            UserInfo user = new UserInfo();
+            user.city = generateCity(locale);
+            user.name = generateName(locale);
+            user.phone = generatePhone(locale);
             return user;
         }
     }
 
-    @Value
     public static class UserInfo {
         String city;
         String name;
         String phone;
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
     }
+
 }
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,4 +1,79 @@
 package ru.netology.delivery.test;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import ru.netology.delivery.data.DataGenerator;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class DeliveryTest {
+
+    String citySelector = "[data-test-id=city] input";
+    String dateSelector = "[data-test-id=date] input";
+    String nameSelector = "[data-test-id=name] input";
+    String phoneSelector = "[data-test-id=phone] input";
+    String agreementSelector = "[data-test-id=agreement] span.checkbox__box";
+    String buttonSelector = "button .button__text";
+    String notificationTitleSelector = "[data-test-id=success-notification] .notification__title";
+    String notificationContentSelector = "[data-test-id=success-notification] .notification__content";
+    String notificationRePlanTitleSelector = "[data-test-id=replan-notification] .notification__title";
+    String notificationRePlanButton = "[data-test-id=replan-notification] button";
+    String sendButtonText = "Запланировать";
+    String notificationTitle = "Успешно!";
+    String notificationContent = "Встреча успешно запланирована на ";
+    String notificationRePlanTitle = "Необходимо подтверждение";
+    String notificationButtonText = "Перепланировать";
+
+
+    void clearField(String selector) {
+        $(selector).sendKeys(Keys.CONTROL + "A");
+        $(selector).sendKeys(Keys.BACK_SPACE);
+    }
+
+    public void stepsToFillForm(String city, String date, String name, String phone) {
+        $(citySelector).setValue(city);
+        clearField(dateSelector);
+        $(dateSelector).setValue(date);
+        $(nameSelector).setValue(name);
+        $(phoneSelector).setValue(phone);
+        $(agreementSelector).click();
+        $(buttonSelector).shouldHave(text(sendButtonText)).click();
+    }
+
+    public void checkNotificationSuccess(String date) {
+        $(notificationTitleSelector).should(visible, Duration.ofSeconds(15)).shouldHave(text(notificationTitle));
+        $(notificationContentSelector).shouldHave(visible, text(notificationContent + date));
+    }
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    @DisplayName("Should successful plan and replan meeting")
+    void shouldSuccessfulPlanAndRePlanMeeting() throws IOException {
+        var validUser = DataGenerator.Registration.generateUser("ru");
+        var daysToAddForFirstMeeting = 4;
+        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
+        var daysToAddForSecondMeeting = 7;
+        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
+
+        stepsToFillForm(validUser.getCity(), firstMeetingDate, validUser.getName(), validUser.getPhone());
+        checkNotificationSuccess(firstMeetingDate);
+        clearField(dateSelector);
+        $(dateSelector).setValue(secondMeetingDate);
+        $(buttonSelector).shouldHave(text(sendButtonText)).click();
+        $(notificationRePlanTitleSelector).should(visible).shouldHave(text(notificationRePlanTitle));
+        $(notificationRePlanButton).should(visible).shouldHave(text(notificationButtonText)).click();
+        checkNotificationSuccess(secondMeetingDate);
+    }
 }
